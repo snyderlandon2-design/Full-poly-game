6e2ae881fd1285198b9c72b103e93e8bb2f57543
snyderlandon2-design <snyder.landon2@icloud.com>
2026-09-17 19:11:01 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,38 @@
   Never put a Supabase service-role key here.
 */
 window.POLYCLASH_CONFIG = {
-  SUPABASE_URL: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY_HERE"
+  SUPABASE_URL: "API Keys
+Configure API keys to securely control access to your project
+Docs
+Publishable and secret API keys
+Legacy anon, service_role API keys
+Your new API keys are here
+We've updated our API keys to better support your application needs. Join the discussion on GitHub
+Having trouble with the new API keys? Contact support
+
+Publishable key
+This key is safe to use in a browser if you have enabled Row Level Security (RLS) for your tables and configured policies.
+
+New publishable key
+NAME	API KEY	
+default
+No description
+sb_publishable_9duf0PXOt1EBaIRB1f0mKg_e4_taIjA
+
+
+Publishable keys can be safely shared publicly
+Secret keys
+These API keys allow privileged access to your project's APIs. Use in servers, functions, workers or other backend components of your application.
+
+New secret key
+NAME	API KEY	
+default
+No description
+sb_secret_abgnm
+••••••••••••••••
+
+
+
+https: "//vzpwonmdcryunfpqvegw.supabase.co/rest/v1/",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJkdG11ZnJvZm5vb2NraHFnd25iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2MjE4NjcsImV4cCI6MjEwNTE5Nzg2N30.0iGlBd6H8siCgdt7WZ6k-vygsyW6aO7Tvwp_2Nm6nOk" 
 };
